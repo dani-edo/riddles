c50a3852dc8152552bb84bcb068d165343e64840
dani_edo <dani.edogawa.17@gmail.com>
2023-11-26 21:59:09 +0700

    fix styling (bg color, use center, use sizedbox, outlined button, etc)

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -11,34 +11,47 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
       return Scaffold(
-          // appBar: AppBar(title: Text('App Bar Ttitle')),
-          body: Container(
-        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-        decoration: BoxDecoration(color: Color(0xFF3625AF)),
-        height: MediaQuery.of(context).size.height > 100.0
-            ? MediaQuery.of(context).size.height
-            : 100.0,
-        width: MediaQuery.of(context).size.width,
-        // height: 100.0,
-        child: Column(children: [
-          Container(
-            width: 300,
-            margin: const EdgeInsets.symmetric(vertical: 100),
-            child: Image.asset('assets/images/quiz-logo.png'),
+        // appBar: AppBar(title: Text('App Bar Ttitle')),
+        body: Container(
+          padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color.fromARGB(255, 78, 13, 151),
+                  Color.fromARGB(255, 135, 17, 186)
+                ]),
           ),
-          Container(
-            margin: const EdgeInsets.only(bottom: 15),
-            child: const Text(
-              'Let\'s Play Fun Quiz',
-              style: TextStyle(color: Colors.white, fontSize: 25),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/images/quiz-logo.png',
+                  width: 300,
+                ),
+                const SizedBox(
+                  height: 100,
+                ),
+                const Text(
+                  'Let\'s Play Fun Quiz',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                OutlinedButton(
+                  onPressed: () {},
+                  style:
+                      OutlinedButton.styleFrom(foregroundColor: Colors.white),
+                  child: const Text('Start'),
+                )
+              ],
             ),
           ),
-          ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(elevation: 3),
-              child: const Text('Play'))
-        ]),
-      ));
+        ),
+      );
     });
   }
 }

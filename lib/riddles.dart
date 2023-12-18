63aacde10f1a1ae510c98f8f4feb03263ff53969
dani_edo <dani.edogawa.17@gmail.com>
2023-12-18 23:20:05 +0700

    clean ternary with variable

--- lib/riddles.dart
+++ lib/riddles.dart
@@ -20,28 +20,29 @@ class _QuizState extends State<Riddles> {
 
   @override
   Widget build(BuildContext context) {
+    final screenAWidget = activeScreen == 'start-screen'
+        ? StartScreen(
+            startQuiz: switchScreen,
+            title: 'Riddle',
+            subTitle: 'Enjoy playing riddle',
+          )
+        : const Question();
     return MaterialApp(
         title: 'Riddle',
         home: Scaffold(
-          // appBar: AppBar(title: Text('App Bar Ttitle')),
-          body: Container(
-              padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Color.fromARGB(255, 78, 13, 151),
-                      Color.fromARGB(255, 135, 17, 186)
-                    ]),
-              ),
-              child: activeScreen == 'start-screen'
-                  ? StartScreen(
-                      startQuiz: switchScreen,
-                      title: 'Riddle',
-                      subTitle: 'Enjoy playing riddle',
-                    )
-                  : const Question()),
-        ));
+            // appBar: AppBar(title: Text('App Bar Ttitle')),
+            body: Container(
+          padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color.fromARGB(255, 78, 13, 151),
+                  Color.fromARGB(255, 135, 17, 186)
+                ]),
+          ),
+          child: screenAWidget,
+        )));
   }
 }

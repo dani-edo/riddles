aa94a3307cefe5ca53f0835cf92a09563a06bfd1
dani_edo <dani.edogawa.17@gmail.com>
2023-12-18 23:14:15 +0700

    refactor change screen method to ternary

--- lib/riddles.dart
+++ lib/riddles.dart
@@ -10,21 +10,11 @@ class Riddles extends StatefulWidget {
 }
 
 class _QuizState extends State<Riddles> {
-  Widget? activeScreen;
-
-  @override
-  void initState() {
-    activeScreen = StartScreen(
-      startQuiz: switchScreen,
-      title: 'Riddle',
-      subTitle: 'Enjoy playing riddle',
-    );
-    super.initState();
-  }
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = const Question();
+      activeScreen = 'question-string';
     });
   }
 
@@ -35,18 +25,23 @@ class _QuizState extends State<Riddles> {
         home: Scaffold(
           // appBar: AppBar(title: Text('App Bar Ttitle')),
           body: Container(
-            padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Color.fromARGB(255, 78, 13, 151),
-                    Color.fromARGB(255, 135, 17, 186)
-                  ]),
-            ),
-            child: activeScreen,
-          ),
+              padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color.fromARGB(255, 78, 13, 151),
+                      Color.fromARGB(255, 135, 17, 186)
+                    ]),
+              ),
+              child: activeScreen == 'start-screen'
+                  ? StartScreen(
+                      startQuiz: switchScreen,
+                      title: 'Riddle',
+                      subTitle: 'Enjoy playing riddle',
+                    )
+                  : const Question()),
         ));
   }
 }
